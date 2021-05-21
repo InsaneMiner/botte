38e44de42c17ce10a4a7f660d0bdac16e630439c
InsaneMiner <gurued58@gmail.com>
2021-05-21 11:58:01 +0200

    Now trying to get into 16 bit mode and out

--- stage1/main.asm
+++ stage1/main.asm
@@ -24,6 +24,10 @@ boot:
 	int 0x13
 
 	cli
+
+	;mov si,hello0
+	;call print_16
+
 	lgdt [gdt_ptr]
 	
 	mov eax, cr0
@@ -31,8 +35,36 @@ boot:
     mov cr0, eax
 
 	jmp 0x8:boot2
+.done:
+    ret
 
-bit32:
+%include "gdt.inc"
+
+disk:
+	db 0x0
+
+hello0: db "dsaf",0
+
+
+times 510 - ($-$$) db 0
+dw 0xaa55
+copy_target:
+
+[bits 32]
+
+bits16:
+    mov ax, 0x20
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    jmp 0x18:.done
+.done:
+    ret
+bits 32
+
+bit32: ; goes into 32 bit mode
   
   mov ax, 0x10
   mov ds, ax
@@ -47,67 +79,14 @@ bit32:
     ret
 
 
-
-bits16:
-    mov ax, 0x20
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    jmp 0x18:.done
-.done:
-    ret
-gdt_start:
-gdt_:
-;null descriptor
-    dd 0
-    dd 0
-;32bit code
-    dw 0xFFFF ; limit value 
-	dw 0x0
-	db 0x0
-	db 10011010b
-	db 11001111b
-	db 0x0
-;32bit data
-    dw 0xFFFF
-	dw 0x0
-	db 0x0
-	db 10010010b
-	db 11001111b
-	db 0x0
-;16bit code
-    dw 0xFFFF ; limit value (2 bytes)
-	dw 0x0    ;    (2 bytes)
-	db 0x0    ;    (1 byte)
-	db 10011010b ; (1 byte)
-    db 00000000b ; (1 byte)
-	db 0x0       ; (1 byte)
-;16bit data
-    dw 0xFFFF
-	dw 0x0
-	db 0x0
-	db 10010010b
-    db 00000000b ; (1 byte)
-	db 0x0
-gdt_end:
-
-gdt_ptr:
-    dw gdt_end - gdt_start - 1
-    dd gdt_start
-disk:
-	db 0x0
-
-
-times 510 - ($-$$) db 0
-dw 0xaa55
-copy_target:
-bits 32
-	hello: db "Hello more than 512 bytes world!!",0
-boot2:
-    call bit32
+hello: db "Hello more than 512 bytes world!!",0
+yo: db "dsd",0
+print_:
 	mov esi,hello
+	mov ebx,0xb8000
+	ret
+print_1:
+	mov esi,yo
 	mov ebx,0xb8000
 .loop:
 	lodsb
@@ -117,6 +96,16 @@ boot2:
 	mov word [ebx], ax
 	add ebx,2
 	jmp .loop
+
+
+boot2:
+    call bit32
+	
+	call print_1
+
+	mov si,hell
+	lea esi, [rel + print_16]
+	call function16
 halt:
 	mov esp,kernel_stack_top
 	extern stage2
@@ -125,9 +114,73 @@ halt:
 	cli
 	hlt
 
+
+[bits 16]
+hell:
+ db "sdfasd",0
+function16:
+	call bits16
+	call RealMode
+	;call esi ; calls the function
+	ret
+
+print_16:
+	mov ah,0x0e
+.loop:
+    lodsb
+    or al,al ; is al == 0 ?
+    jz halt  ; if (al == 0) jump to halt label
+    int 0x10 ; runs BIOS interrupt 0x10 - Video Services
+    jmp .loop
+
+
 [BITS 32]
 global print_what
 global print_yo
+
+
+[bits 16]
+ 
+idt_real:
+	dw 0x3ff		; 256 entries, 4b each = 1K
+	dd 0			; Real Mode IVT @ 0x0000
+ 
+savcr0:
+	dd 0			; Storage location for pmode CR0.
+ 
+RealMode:
+        ; We are already in 16-bit mode here!
+ 
+	cli			; Disable interrupts.
+ 
+	; Need 16-bit Protected Mode GDT entries!
+	mov eax, 0x18	; 16-bit Protected Mode data selector.
+	mov ds, eax
+	mov es, eax
+	mov fs, eax
+	mov gs, eax
+	mov ss, eax
+ 
+ 
+	; Disable paging (we need everything to be 1:1 mapped).
+	mov eax, cr0
+	mov [savcr0], eax	; save pmode CR0
+	and eax, 0x7FFFFFFe	; Disable paging bit & disable 16-bit pmode.
+	mov cr0, eax
+ 
+	jmp 0:GoRMode		; Perform Far jump to set CS.
+ 
+GoRMode:
+	mov sp, 0x8000		; pick a stack pointer.
+	mov ax, 0		; Reset segment registers to 0.
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	lidt [idt_real]
+	sti	
+[bits 32]
 
 section .bss
 align 4
